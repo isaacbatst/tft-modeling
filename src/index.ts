d796ffbd40f5e4cfc32bfc123fbd1fe2b120983c
Isaac Batista <isaacbatst@gmail.com>
2022-08-12 21:57:18 -0300

    :construction: test game on index

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log('DAWN OF TFT MODEL');
+import {Game
+  , GameDeck, IGamePlayer} from './domain/entities/Game/Game';
+
+class DeckMock implements GameDeck {
+  takeRandomHand = () => [];
+}
+
+class PlayerMock implements IGamePlayer {
+  constructor(private id: string) {}
+
+  getId(): string {
+    return this.id;
+  }
+
+  setHand = () => {};
+  setGold = () => {};
+  getGold = () => 10;
+}
+
+const player1 = new PlayerMock('any-id-1');
+const player2 = new PlayerMock('any-id-2');
+const deck = new DeckMock();
+
+const players = [player1, player2];
+
+const game = new Game(players, deck);
+
+game.start();
